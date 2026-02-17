be65dce1788ade9e27e3b806a40777a8bfb68509
pa7139-lgtm <pa7139@srmist.edu.in>
2026-02-17 11:14:35 +0530

    Add files via upload

---
+++ OOPSBannerapp2.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   ***        ***      ******        ***** "));
+        System.out.println(String.join(" "," **   **    **   **    **    **     **     "));
+        System.out.println(String.join(" ","**     **  **     **   **     **   **     "));
+        System.out.println(String.join(" ","**     **  **     **   **    **     **  "));
+        System.out.println(String.join(" ","**     **  **     **   ******        ***"));
+        System.out.println(String.join(" ","**     **  **     **   **              **  "));
+        System.out.println(String.join(" ","**     **  **     **   **               **     "));
+        System.out.println(String.join(" "," **   **    **    **   **              **       "));
+        System.out.println(String.join(" ","   ***        ***      **         *****    "));
+
+    }
+}
